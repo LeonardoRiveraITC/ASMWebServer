c2c665972b82273cc113a4d6610fec61d0b7e6f8
LeonardoRiveraITC <19030342@itcelaya.edu.mx>
2025-07-12 14:42:38 -0600

    FIX: receive 16 bytes of data from client

--- server.s
+++ server.s
@@ -28,10 +28,15 @@ _start:
 	mov rsi,0
 	mov rdx,0
 	syscall
-	#write
 	mov rdi,rax
+	#read
+	mov rax,0
+	mov rsi,0
+	mov rdx,16
+	syscall
+	#write
 	mov rax,1
-	sub rsp,23 #HTTP/1.0 200 OK
+	sub rsp,19 #HTTP/1.0 200 OK
                 mov BYTE PTR [rsp], 0x48
                 mov BYTE PTR [rsp+1], 0x54
                 mov BYTE PTR [rsp+2], 0x54
@@ -47,16 +52,15 @@ _start:
                 mov BYTE PTR [rsp+12], 0x20
                 mov BYTE PTR [rsp+13], 0x4F
                 mov BYTE PTR [rsp+14], 0x4B
-                mov BYTE PTR [rsp+15], 0x5C
-                mov BYTE PTR [rsp+16], 0x72
-                mov BYTE PTR [rsp+17], 0x5C
-                mov BYTE PTR [rsp+18], 0x6E
-                mov BYTE PTR [rsp+19], 0x5C
-                mov BYTE PTR [rsp+20], 0x72
-                mov BYTE PTR [rsp+21], 0x5C
-                mov BYTE PTR [rsp+22], 0x6E
+                mov BYTE PTR [rsp+15], 0x0D
+                mov BYTE PTR [rsp+16], 0x0A
+                mov BYTE PTR [rsp+17], 0x0D
+                mov BYTE PTR [rsp+18], 0x0A
 	mov rsi,rsp
-	mov rdx,23
+	mov rdx,19
+	syscall
+	#close
+	mov rax,3
 	syscall
 
 end:
