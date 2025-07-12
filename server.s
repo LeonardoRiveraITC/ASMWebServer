9ef3a2160aa8c0603e81c86c2f9cd6ee606c8a6e
LeonardoRiveraITC <19030342@itcelaya.edu.mx>
2025-07-12 12:53:17 -0600

    200 OK

--- server.s
+++ server.s
@@ -28,6 +28,36 @@ _start:
 	mov rsi,0
 	mov rdx,0
 	syscall
+	#write
+	mov rdi,rax
+	mov rax,1
+	sub rsp,23 #HTTP/1.0 200 OK
+                mov BYTE PTR [rsp], 0x48
+                mov BYTE PTR [rsp+1], 0x54
+                mov BYTE PTR [rsp+2], 0x54
+                mov BYTE PTR [rsp+3], 0x50
+                mov BYTE PTR [rsp+4], 0x2F
+                mov BYTE PTR [rsp+5], 0x31
+                mov BYTE PTR [rsp+6], 0x2E
+                mov BYTE PTR [rsp+7], 0x30
+                mov BYTE PTR [rsp+8], 0x20
+                mov BYTE PTR [rsp+9], 0x32
+                mov BYTE PTR [rsp+10], 0x30
+                mov BYTE PTR [rsp+11], 0x30
+                mov BYTE PTR [rsp+12], 0x20
+                mov BYTE PTR [rsp+13], 0x4F
+                mov BYTE PTR [rsp+14], 0x4B
+                mov BYTE PTR [rsp+15], 0x5C
+                mov BYTE PTR [rsp+16], 0x72
+                mov BYTE PTR [rsp+17], 0x5C
+                mov BYTE PTR [rsp+18], 0x6E
+                mov BYTE PTR [rsp+19], 0x5C
+                mov BYTE PTR [rsp+20], 0x72
+                mov BYTE PTR [rsp+21], 0x5C
+                mov BYTE PTR [rsp+22], 0x6E
+	mov rsi,rsp
+	mov rdx,23
+	syscall
 
 end:
         mov rax,60
