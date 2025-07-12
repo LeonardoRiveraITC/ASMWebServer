95c581bcad0bc60126a044bb4194677fdab42832
LeonardoRiveraITC <19030342@itcelaya.edu.mx>
2025-07-12 11:55:07 -0600

    accept connections

--- server.s
+++ server.s
@@ -1,30 +1,36 @@
 .intel_syntax noprefix
 .global _start
 _start:
-	#socket
-	mov rax,41
-	mov rdi,2 #AF_INET
-	mov rsi,1 #SOCK_STREAM
+        #socket
+        mov rax,41
+        mov rdi,2 #AF_INET
+        mov rsi,1 #SOCK_STREAM
+        mov rdx,0
+        syscall
+
+        #bind
+        mov rdi,rax
+        mov rax,49
+        sub rsp,16 #SOCKADDR_IN
+                mov WORD PTR [rsp],2 #sin_family
+                mov WORD PTR [rsp+2],0x5000
+                mov DWORD PTR[rsp+4],0x0
+                mov BYTE PTR[rsp+8],0x0
+        mov rsi,rsp
+        mov rdx,16
+        syscall
+        #listen
+        mov rax,50
+        mov rsi,0
+        syscall
+	#accept
+	mov rax,43
+	mov rsi,0
 	mov rdx,0
-	syscall
-	
-	#bind
-	mov rdi,rax
-	mov rax,49
-	sub rsp,16 #SOCKADDR_IN
-		mov WORD PTR [rsp],2 #sin_family
-		mov WORD PTR [rsp+2],0x5000
-		mov DWORD PTR[rsp+4],0x0
-		mov BYTE PTR[rsp+8],0x0
-	mov rsi,rsp
-	mov rdx,16
-	syscall
-	#listen
-	mov rax,50
-	mov rsi,10
 	syscall
 
 end:
-	mov rax,60
-	mov rdi,0
-	syscall
+        mov rax,60
+        mov rdi,0
+        syscall
+
